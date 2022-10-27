a8f5539b43e4a5f958898a8365edf75cdc6dfa88
Alejandro Guerrero Martínez <alejandro.guerrero@bravezebra.es>
2022-10-27 17:20:36 +0200

    Added random matrix function

---
+++ rendom_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import random
+
+def random_matrix(array):
+    a = random.randint(0, 50)
+    b = random.randint(0, 50)
+    c = random.randint(0, 50)
+    d = random.randint(0, 50)
+    M = [[a,b],[c,d]]
+    return(M)
